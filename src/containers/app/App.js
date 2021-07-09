9475439dfe19dd951da0ef0efacf46454fd2e501
Lázaro Alves <geo.eolo@gmail.com>
2021-07-08 23:25:20 -0300

    Adiciona estado a aplicação

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,12 +1,35 @@
 import React, {Component} from 'react';
 
+import Button from '../../components/button';
+import Input from '../../components/input';
+import List from '../../components/list';
+
 class App extends Component {
   state  = {
     input: ''
   };
 
-  render() {
-    return <h1>Hey</h1>    
+  handleOnClick = () => {
+    console.log('Button was clicked');
+  }
+
+  handleOnChange = event => {
+    // console.log('Input was changed', event.target.value);
+
+    this.setState({ input: event.target.value });
+
+    console.log(this.state);
+  }
+
+  render() { 
+    const { input } = this.state;
+    return (
+      <div>
+        <List todoList={[]} />
+        <Input onChange={() => this.handleOnChange()} value={input} />
+        <Button onClick={() => this.handleOnClick()}>Adicionar</Button>
+      </div>
+    );    
   }
 }
 
